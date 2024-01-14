6f33f49e16c120a2366498a1e37b0ea91131ccc0
KevinHarbour <kharbour22@gmail.com>
2024-01-14 12:54:08 -0700

    nooodes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Kevin is the champion";
